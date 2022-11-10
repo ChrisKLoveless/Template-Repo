54abcc9792cda4db92a2aa95db9c5d81dd2b0388
Chris Loveless <chriskloveless@gmail.com>
2022-11-09 20:02:16 -0800

    add asset folder and html test modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin'); 
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,15 +9,15 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devServer: {                 
-    contentBase: './dist'      
+  devServer: {
+    contentBase: './dist'
   },
   devtool: 'eval-source-map',
   plugins: [
     new ESLintPlugin(),
     new CleanWebpackPlugin({
       verbose: true
-    }), 
+    }),
     new HtmlWebpackPlugin({
       title: 'Shape Tracker',
       template: './src/index.html',
@@ -32,7 +32,26 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
-      }
+      },
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+
     ]
   }
 };
